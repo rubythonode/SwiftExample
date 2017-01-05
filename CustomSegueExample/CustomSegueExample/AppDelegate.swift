3b71f88843eb95a101f885e8335936d36ded6fc0
구봉회 <wantrnqhdghl@naver.com>
2017-01-06 02:45:17 +0900

    default swift project files

---
+++ CustomSegueExample/CustomSegueExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomSegueExample
+//
+//  Created by 구봉회 on 06/01/2017.
+//  Copyright © 2017 구봉회. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CustomSegueExample/CustomSegueExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CustomSegueExample
+//
+//  Created by 구봉회 on 06/01/2017.
+//  Copyright © 2017 구봉회. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
